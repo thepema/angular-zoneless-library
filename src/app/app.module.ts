b08b59ca67c97c3e8b041c70e34c65b21a77f4b7
Pema <pema7284@gmail.com>
2023-07-06 16:38:50 +0200

    sandbox de pruebas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
 import { GrandComponent } from './grand/grand.component';
-import { ZonelessModule } from 'src/zoneless/zoneless.module';
+import { ZonelessModule } from 'zoneless-library';
 
 @NgModule({
   declarations: [
